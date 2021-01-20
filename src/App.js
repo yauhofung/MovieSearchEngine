3a6dfaae2b50c3c89ef07ecc48fa103c40a0d337
yauhofung <yauho.fung@student.mec.cuny.edu>
2021-01-20 14:45:34 -0500

    Added React boilerplate

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from '@material-ui/core';
+
+class App extends React.Component {
+	render() {
+		return (
+			<Container>
+
+			</Container>
+		);
+	}
+}
+
+export default App;

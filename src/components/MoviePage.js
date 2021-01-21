636fc652949fd22d58e0116c9fa552e51d99d42e
ddesir <d.desir@gmail.com>
2021-01-21 18:00:46 -0500

    Initial commit

---
+++ src/components/MoviePage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+
+const MoviePage = () => {
+    <Grid item>
+        <Typography variant='h2'>MovieTitle</Typography>
+    </Grid>
+
+    <Grid item>
+        <img src={} />
+    </Grid>
+
+    <Grid item>
+        <Typography variant='body'>
+            
+        </Typography>
+    </Grid>
+
+}

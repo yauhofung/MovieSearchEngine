b92d090dbd4030becdf21bdc18f5ad0443453fe0
yauhofung <yauho.fung@student.mec.cuny.edu>
2021-01-21 13:21:07 -0500

    formatted all files and organized imports

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
 import React from 'react';
+
 import { Container, Button } from '@material-ui/core';
-import Navbar from './components/Navbar';
-import SearchBar from './components/SearchBar';
-import {
-	Switch,
-	Route,
-	Link
-} from "react-router-dom";
-import Test from './Test';
+import { Switch, Route, Link } from "react-router-dom";
 import KeyboardBackspaceIcon from '@material-ui/icons/KeyboardBackspace';
+
+import Navbar from './components/Navbar';
+import Test from './components/Test';
+import SearchBar from './components/SearchBar';
 
 class App extends React.Component {
 
 	render() {
 		return (
-			<div>
+			<>
 				<Navbar />
 				<Container>
 
 					{/* The Link to="" */}
-
 					<Link to="/Test">
-						<Button style={{color: 'black', margin: '5px 0 0 5px' }}>
+						<Button style={{ color: 'black', margin: '5px 0 0 5px' }}>
 							<KeyboardBackspaceIcon />
 						</Button>
 					</Link>
+
 					<SearchBar />
 
 					{/* The Switch */}
@@ -37,7 +35,7 @@ class App extends React.Component {
 					</Switch>
 
 				</Container>
-			</div>
+			</>
 		);
 	}
 }
--- src/theme.js
+++ src/theme.js
@@ -1,14 +1,14 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 
 const theme = createMuiTheme({
-    palette: {
-        primary: {
-            main: '#223441',
-        },
-        secondary: {
-            main: '#FFC173',
-        },
-    },
+	palette: {
+		primary: {
+			main: '#223441',
+		},
+		secondary: {
+			main: '#FFC173',
+		},
+	},
 });
 
 export default theme;

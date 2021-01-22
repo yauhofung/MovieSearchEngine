fcb3b1521e57ee2efe2f1584d8850c1d8e0c37c3
yauhofung <yauho.fung@student.mec.cuny.edu>
2021-01-21 20:45:45 -0500

    Added Textfields, Button, and Grid

--- src/pages/AdvancedSearch.js
+++ src/pages/AdvancedSearch.js
@@ -1,24 +1,55 @@
 import React from 'react';
-import { Container, TextField } from '@material-ui/core';
+
+import { Button, Grid, TextField } from '@material-ui/core';
 
 class AdvancedSearch extends React.Component {
+	// calls TMDb API with advanced searches
+	submitForm(event) {
+		event.preventDefault();
+	}
 	render() {
 		return (
-			<div>
-				<Container>
-					<h4>
-						{/* Needs more textFields for advanced search */}
+			<form onSubmit={(event) => { this.submitForm(event); }}>
+				<Grid container spacing={1}>
+					<Grid item xs={12} md={6}>
+						<TextField
+							variant='outlined'
+							label='Title'
+							placeholder='Enter movie title'
+							fullWidth
+						/>
+					</Grid>
+					<Grid item xs={12} md={6}>
 						<TextField
 							variant='outlined'
 							label='Genre'
-
+							placeholder='Enter movie genre'
+							fullWidth
 						/>
-
-						{/* Advanced submit button goes here */}
-
-					</h4>
-				</Container>
-			</div>
+					</Grid>
+					<Grid item xs={12} md={6}>
+						<TextField
+							variant='outlined'
+							label='Release Year'
+							placeholder='Enter movie release year'
+							fullWidth
+						/>
+					</Grid>
+					<Grid item xs={12} md={6}>
+						<TextField
+							variant='outlined'
+							label='ID'
+							placeholder='Enter movie ID'
+							fullWidth
+						/>
+					</Grid>
+					<Grid item xs={12}>
+						<Button type='submit' color="primary" variant='contained' fullWidth>
+							Search
+						</Button>
+					</Grid>
+				</Grid>
+			</form>
 		);
 	}
 }

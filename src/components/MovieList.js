79eccd44466154788bc71146f432da3b71fca502
ddesir <d.desir@gmail.com>
2021-01-22 11:17:07 -0500

    Return MoviePage component

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import Container from '@material-ui/core/Container'
+import Container from '@material-ui/core/Container';
 import { Grid } from '@material-ui/core';
+import MoviePage from './MoviePage';
 
 const MovieList = (props) => {
-    return (
-        <Grid container direction='row'>
-            <Grid item direction='column' sm={12}>
-                
-            </Grid>
+    <Grid container direction='row'>
+        <Grid item direction='column' sm={12}>
+            {
+                props.movies.map((movie, i) => {
+                    return (
+                        <MoviePage key={i} image={movie.poster_path} />
+                    );
+                })
+            }
         </Grid>
-    )
+    </Grid>
 }
 
 export default MovieList;

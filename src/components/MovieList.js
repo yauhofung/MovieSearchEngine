c353c3ab6cfc3edc0646d4dbfff2e3d29a2c6005
ddesir <d.desir@gmail.com>
2021-01-22 11:02:17 -0500

    Initial Grid structure

---
+++ src/components/MovieList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Container from '@material-ui/core/Container'
+import { Grid } from '@material-ui/core';
+
+const MovieList = (props) => {
+    return (
+        <Grid container direction='row'>
+            <Grid item direction='column' sm={12}>
+                
+            </Grid>
+        </Grid>
+    )
+}
+
+export default MovieList;

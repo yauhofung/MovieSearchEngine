0dc5b655e2a134072649d495cbbf5b480d262941
ddesir <d.desir@gmail.com>
2021-01-22 14:02:28 -0500

    Movie posters from search

--- src/components/MoviePage.js
+++ src/components/MoviePage.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import SearchBar from './SearchBar';
+import defaultImage from '../img/default-movie.png'
 
-const MoviePage = () => {
+const MoviePage = (props) => {
     return (
     <Grid container>
         <Grid item>
-            <Typography variant='h2'>MovieTitle</Typography>
+            {
+                props.image == null ? <img src={defaultImage} alt={'card image'} /> : <img src={`http://image.tmdb.org/t/p/w185${props.image}`} alt={'card image'} />
+            }
         </Grid>
 
         <Grid item>

c411a8c231d439e136db6702afecab346f78e6e0
yauhofung <yauho.fung@student.mec.cuny.edu>
2021-01-21 19:46:08 -0500

    Moved Test.js into components

---
+++ src/components/Test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Container, Typography } from '@material-ui/core';
+
+class Test extends React.Component {
+	render() {
+		return (
+			<Container>
+				<Typography>
+					Hello World!
+				</Typography>
+			</Container>
+		);
+	}
+}
+
+export default Test;

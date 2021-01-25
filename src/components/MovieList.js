25c5ec3b5460192be916fc44f12eabafe222aa1e
yauhofung <yauho.fung@student.mec.cuny.edu>
2021-01-25 14:50:42 -0500

    Fixed search result Grid

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -5,16 +5,16 @@ import MoviePage from './MoviePage';
 const MovieList = (props) => {
 	return (
 		<Grid container>
-			<Grid item xs={12} sm={6} md={4} lg={3}>
-				{
-					props.movies.map((movie, i) => {
-						return (
-							//passing in props to MoviePage component
+			{
+				props.movies.map((movie, i) => {
+					return (
+						<Grid item xs={12} sm={6} md={4} lg={3}>
+							{/* passing in props to MoviePage component */}
 							<MoviePage key={i} title={movie.title} image={movie.poster_path} />
-						);
-					})
-				}
-			</Grid>
+						</Grid>
+					);
+				})
+			}
 		</Grid>
 	);
 }

8835f458b55adf08d6a6e68e5f9c4cfa5f3ccde7
yauhofung <yauho.fung@student.mec.cuny.edu>
2021-01-25 15:22:18 -0500

    Centered movie posters

--- src/components/MoviePage.js
+++ src/components/MoviePage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Typography, Card, CardContent } from '@material-ui/core';
+import { Grid, Typography, Card, CardContent } from '@material-ui/core';
 
 import defaultImage from '../img/default-movie.png';
 
@@ -8,15 +8,21 @@ const MoviePage = (props) => {
 	return (
 		<Card>
 			<CardContent>
-				{
-					//if props image is null:
-					props.image == null ?
-						//show the default image or the api's image prop.
-						<img src={defaultImage} alt={'unavailable movie poster'} /> :
-						<img src={`http://image.tmdb.org/t/p/w200${props.image}`} alt={'card movie poster'} />
-				}
+				{/* centers the movie posters */}
+				<Grid container>
+					<Grid item xs></Grid>
+					<Grid item xs>
+						{
+							//if props image is null:
+							props.image === null ?
+								//show the default image or the api's image prop.
+								<img src={defaultImage} alt={'unavailable movie poster'} /> :
+								<img src={`http://image.tmdb.org/t/p/w200${props.image}`} alt={'card movie poster'} />
+						}
+					</Grid>
+					<Grid item xs></Grid>
+				</Grid>
 
-				{/* displaying the card */}
 				<Typography align='center'>
 					{props.title}
 				</Typography>

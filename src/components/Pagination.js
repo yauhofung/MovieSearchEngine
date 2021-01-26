f12a86190b8eedad2486973f04c8d3cb3cca86e1
ddesir <d.desir@gmail.com>
2021-01-26 11:09:33 -0500

    Accepted incoming changes

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -9,33 +9,6 @@ const Pagination = (props) => {
 		let active = props.currentPage === i ? 'active' : '';
 
 		pageLinks.push(
-<<<<<<< HEAD
-			<Grid
-				className={`waves-effect ${active}`}
-				direction="row"
-				justify="center"
-				alignItems="center"
-				
-
-				key={i}
-				onClick={() => props.nextPage(i)}>
-				<Button href="#">
-					{i}
-				</Button>
-
-			</Grid>)
-	}
-
-	return (
-		<Grid 
-		container
-		direction="row"
-		justify="center"
-		alignItems="center"
-		
-
-		spacing={2}>
-=======
 			<Grid className={`waves-effect ${active}`} direction="row" alignItems="right" key={i} onClick={() => props.nextPage(i)}>
 				<Button href="#">
 					{i}
@@ -46,7 +19,6 @@ const Pagination = (props) => {
 
 	return (
 		<Grid container direction="row" alignItems="center" spacing={2}>
->>>>>>> beff7acb91acb25a491d4472288ee69dacdecfcb
 			<Grid container item xs={12} sm={12}>
 				{pageLinks}
 			</Grid>

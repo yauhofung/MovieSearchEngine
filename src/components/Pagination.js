beff7acb91acb25a491d4472288ee69dacdecfcb
yauhofung <yauho.fung@student.mec.cuny.edu>
2021-01-26 10:11:33 -0500

    Removed redundant page links

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -3,50 +3,22 @@ import React from 'react';
 import { Button, Grid } from '@material-ui/core';
 
 const Pagination = (props) => {
-	const pageLinks = []
+	const pageLinks = [];
 
 	for (let i = 1; i <= props.pages + 1; i++) {
 		let active = props.currentPage === i ? 'active' : '';
 
 		pageLinks.push(
-			<Grid
-				className={`waves-effect ${active}`}
-				direction="row"
-				alignItems="right"
-				key={i}
-				onClick={() => props.nextPage(i)}>
+			<Grid className={`waves-effect ${active}`} direction="row" alignItems="right" key={i} onClick={() => props.nextPage(i)}>
 				<Button href="#">
 					{i}
 				</Button>
-
-			
-				|<Button href="#">
-					{i+1}
-				</Button>
-
-				|<Button href="#">
-					{i+2}
-				</Button>
-
-			
-				|<Button href="#">
-					{i+3}
-				</Button>
-
-				|<Button href="#">
-					{i+4}
-				</Button>
-
-
-			</Grid>)
+			</Grid>
+		)
 	}
 
 	return (
-		<Grid 
-		container
-		direction="row"
-		alignItems="center"
-		spacing={2}>
+		<Grid container direction="row" alignItems="center" spacing={2}>
 			<Grid container item xs={12} sm={12}>
 				{pageLinks}
 			</Grid>
@@ -54,4 +26,4 @@ const Pagination = (props) => {
 	)
 }
 
-export default Pagination
+export default Pagination;

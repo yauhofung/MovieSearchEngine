ba92ceb3184e165895ca6023d730a3fb96e076c2
ddesir <d.desir@gmail.com>
2021-01-27 13:42:19 -0500

    Attempted added pagination. Can't read props...Get

--- src/pages/Results.js
+++ src/pages/Results.js
@@ -2,15 +2,26 @@ import React from 'react';
 // imported props here
 import { withRouter } from 'react-router-dom';
 import MovieList from '../components/MovieList';
+import Pagination from '../components/Pagination';
 
 const Results = (props) => {
     // console logs all the stuff assigned from SearchBar
     console.log(props.history.location)
+    const numberPage = Math.floor(props.history.location.getProps.totalResultsGet / 20);
 
     return (
 	<div>
         {/* linked to movies, movies gets the [..data.results] */}
         <MovieList movies={props.history.location.getProps.movieGet} />
+        {
+					this.state.totalResults > 20 ?
+						<Pagination
+							pages={numberPage}
+							nextPage={this.nextPage}
+							currentPage={props.history.location.getProps.currentPageGet}
+						/> :
+						''
+		}
 	</div>
     )
 }

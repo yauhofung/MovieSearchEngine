39ab0be950a50ef95ebe4e7cdd55fcbfd735125b
Jon Chin <jon@sharebyteaching.org>
2021-01-27 14:55:58 -0500

    swapped out this.state for props; added basic error handling for direct loading of page

--- src/pages/Results.js
+++ src/pages/Results.js
@@ -7,23 +7,25 @@ import Pagination from '../components/Pagination';
 const Results = (props) => {
     // console logs all the stuff assigned from SearchBar
     console.log(props.history.location)
-    const numberPage = Math.floor(props.history.location.getProps.totalResultsGet / 20);
-
-    return (
-	<div>
-        {/* linked to movies, movies gets the [..data.results] */}
-        <MovieList movies={props.history.location.getProps.movieGet} />
-        {
-					this.state.totalResults > 20 ?
-						<Pagination
-							pages={numberPage}
-							nextPage={this.nextPage}
-							currentPage={props.history.location.getProps.currentPageGet}
-						/> :
-						''
+    if(!props.history.location.getProps){
+	return(<p>please load from search bar</p>);
+    }else{
+	const numberPage = Math.floor(props.history.location.getProps.totalResultsGet / 20);
+	return (
+	    <div>
+		<MovieList movies={props.history.location.getProps.movieGet} />
+		{
+		    props.history.location.getProps.totalResults > 20 ?
+								   <Pagination
+								       pages={numberPage}
+								   nextPage={this.nextPage}
+								   currentPage={props.history.location.getProps.currentPageGet}
+								   /> :
+								   ''
 		}
-	</div>
-    )
+	    </div>
+	)
+    }
 }
 
 // with router stuff
